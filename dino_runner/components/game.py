44f8147ec6648c26ea745e70491003787d337284
Fernando Marques dos Santos <89764537+TakeruuDEV@users.noreply.github.com>
2022-10-24 11:19:28 -0300

    Add function jump to dino

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,7 +1,7 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
-
+from dino_runner.components.dinosaur import Dinosaur
 
 class Game:
     def __init__(self):
@@ -9,6 +9,7 @@ class Game:
         pygame.display.set_caption(TITLE)
         pygame.display.set_icon(ICON)
         self.screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+        self.player = Dinosaur()
         self.clock = pygame.time.Clock()
         self.playing = False
         self.game_speed = 20
@@ -30,12 +31,14 @@ class Game:
                 self.playing = False
 
     def update(self):
-        pass
+        user_input = pygame.key.get_pressed()
+        self.player.update(user_input)
 
     def draw(self):
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
