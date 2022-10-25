1ecb68670c9c493765e62a83bf8ecd35dd4d73a8
Fernando Marques dos Santos <89764537+TakeruuDEV@users.noreply.github.com>
2022-10-25 11:27:51 -0300

    Add Large Cactus to game

--- dino_runner/components/obstacles/obstacle_manager.py
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -1,17 +1,23 @@
 
-from pickle import APPEND
 from dino_runner.components.obstacles.cactus import Cactus
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.components.obstacles.LargeCactus import LargeCactus
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 import pygame
+import random
 
 class ObstacleManager:
     def __init__(self):
         self.obstacles = []
 
     def update(self, game):
+        
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
-            
+            if random.randint(0,1) == 0:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            elif random.randint(0,1) == 1:
+                self.obstacles.append(LargeCactus(LARGE_CACTUS))      
+
+
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
 
